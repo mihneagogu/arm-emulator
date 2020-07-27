63891556a783b185e996d03b5136bc9a291cdfe8
Mihnea <mrg119@ic.ac.uk>
2020-07-27 23:50:08 +0300

    data proc instr almost done

--- src/emulator/data_proc_instr.rs
+++ src/emulator/data_proc_instr.rs
@@ -1,39 +1,73 @@
-use crate::emulator::{em_utilities as util, barrel_shifter as shifter};
-use util::*;
+use crate::emulator::{barrel_shifter as shifter, em_utilities as util};
 use shifter::*;
+use util::*;
 
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
 
-
-/// Returns whether the immediate is enabled for the given instruction
-fn immediate_enabled(bits: u32) -> bool {
-    bit_mask(bits, BitPos32::from_u8(25)) != 0
+#[derive(Debug, FromPrimitive)]
+enum DataProcOpcode {
+    AND = 0,
+    EOR = 1,
+    SUB = 2,
+    RSB = 3,
+    ADD = 4,
+    TST = 8,
+    TEQ = 9,
+    CMP = 10,
+    ORR = 12,
+    MOV = 13,
 }
 
-fn cpsr_enabled(bits: u32) -> bool {
-    bit_mask(bits, BitPos32::from_u8(20)) != 0
+
+macro_rules! immediate_enabled {
+    ($bits:expr) => {
+        bit_mask($bits, bp32![25]) != 0
+    };
 }
 
-fn opcode_bits(bits: u32) -> u32 {
-    process_mask(bits, bp32(21), bp32(24))
+
+
+macro_rules! cpsr_enabled {
+    ($bits:expr) => {
+        bit_mask($bits, bp32![20]) != 0
+    };
+
 }
 
-fn dest_reg(bits: u32) -> u32 {
-    process_mask(bits, bp32(12), bp32(15))
+
+macro_rules! dest_reg {
+    ($bits:expr) => {
+        mask![$bits, 12, 15]
+    };
+
 }
 
-fn operand1_reg_bits(bits: u32) -> u32 {
-    process_mask(bits, bp32![16], bp32![19])
+macro_rules! operand1_reg_bits {
+    ($bits:expr) => {
+        mask![$bits, 16, 19];
+    }
 }
 
-fn operand2_reg_bits(bits: u32) -> u32 {
-    process_mask(bits, bp32![0], bp32![11])
+
+macro_rules! operand2_reg_bits {
+    ($bits:expr) => {
+        mask![$bits, 0, 11]
+    }
+}
+
+/// Gets the opcode bits
+macro_rules! opcode_bits {
+    ($bits:expr) => {
+        mask![$bits, 21, 24]
+    };
 }
 
 /// Executes a data processing instruction
 fn execute_data_processing_instr(instr: &Instruction, cpu: &mut CpuState) {
     let bits = instr.code;
-    let mut operand1: u32 = cpu.registers[operand1_reg_bits(bits) as usize];
-    let mut operand2: u32 = operand2_reg_bits(bits);
+    let mut operand1: u32 = cpu.registers[operand1_reg_bits![bits] as usize];
+    let mut operand2: u32 = operand2_reg_bits![bits];
 
     // will be the computed result that is written into the dest_register
     let mut result: u32 = 0;
@@ -43,11 +77,77 @@ fn execute_data_processing_instr(instr: &Instruction, cpu: &mut CpuState) {
     let mut c_bit: u8 = 0;
 
     // Compute operand2
-    if immediate_enabled(bits) {
+    if immediate_enabled![bits] {
         operand2 = process_mask(bits, bp32![0], bp32![7]);
         operand2 = rotate_right(operand2, process_mask(bits, bp32![8], bp32![11]) * 2);
         c_bit = ((operand2 >> (process_mask(bits, bp32![8], bp32![11]) * 2)) as u8) & 1;
     } else {
         operand2 = reg_offset_shift(cpu, &instr, &mut c_bit);
+    }
+
+    let opcode = opcode_bits![bits];
+    let opcode = FromPrimitive::from_u32(opcode).unwrap();
+
+    match opcode {
+        DataProcOpcode::AND => {
+            result = operand1 & operand2;
+        }
+        DataProcOpcode::EOR => {
+            result = operand1 ^ operand2;
+        }
+        DataProcOpcode::SUB => {
+            result = operand1 - operand2;
+            // set c_bit if op2 > op1 (overflow)
+            c_bit = if operand2 > operand1 { 0 } else { 1 };
+        }
+        DataProcOpcode::RSB => {
+            result = operand2 - operand1;
+            c_bit = if operand1 > operand2 { 0 } else { 1 };
+        }
+        DataProcOpcode::ADD => {
+            result = operand1 + operand2;
+            // set c_bit if it overflows
+            let overflow_check: u64 = (operand1 as u64) + (operand2 as u64);
+            c_bit = if overflow_check >= ((1 as u64) << 32) { 1 } else { 0 };
+        }
+        DataProcOpcode::TST => {
+            result = operand1 & operand2;
+            write_result = 0;
+        }
+        DataProcOpcode::TEQ => {
+            result = operand1 ^ operand2;
+            write_result = 0;
+        }
+        DataProcOpcode::CMP => {
+            result = operand1 - operand2;
+            c_bit = if operand2 > operand1 { 0 } else { 1 };
+        }
+        DataProcOpcode::ORR => {
+            result = operand1 | operand2;
+        }
+        DataProcOpcode::MOV => {
+            result = operand2;
+        }
+    }
+
+    if write_result != 0 {
+        cpu.registers[dest_reg![bits] as usize] = result;
+    }
+
+    // CPSR flags
+
+    if cpsr_enabled![bits] {
+        // TODO: C bit (bit 29 CPSR) - set to c_bit which is determined by the opcode:
+        //set_CPSR_flag(cpu_state, C, c_bit);
+//
+        ////Z bit (bit 30 of CPSR) - Z is 1 iff result == 0:
+        //if (result == 0) {
+            //set_CPSR_flag(cpu_state, Z, true);
+        //} else {
+            //set_CPSR_flag(cpu_state, Z, false);
+        //}
+//
+        ////N bit (bit 31 of CPSR) - set to bit 31 of result:
+        //set_CPSR_flag(cpu_state, N, process_mask(result, 31, 31));
     }
 }
