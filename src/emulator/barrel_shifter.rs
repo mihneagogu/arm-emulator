2107b1039c39a3bfa94d743df54e5d66bf12650d
Mihnea <mrg119@ic.ac.uk>
2020-07-27 21:47:49 +0300

    finished barrel shifter

--- src/emulator/barrel_shifter.rs
+++ src/emulator/barrel_shifter.rs
@@ -1,11 +1,21 @@
 use crate::emulator::em_utilities as util;
 use util::*;
 
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
+
+#[derive(FromPrimitive, Debug)]
 pub enum ShiftOp {
-    LSL,
-    LSR,
-    ASR,
-    ROR,
+    LSL = 0,
+    LSR = 1,
+    ASR = 2,
+    ROR = 3,
+}
+
+macro_rules! mask {
+    ($bits:expr, $start:expr, $end:expr) => {
+        process_mask($bits, bp32![$start], bp32![$end])
+    }
 }
 
 fn shifted_reg_m_bits(bits: u32) -> u32 {
@@ -16,54 +26,83 @@ fn shift_mode_bit(bits: u32) -> bool {
     bit_mask(bits, bp32![4]) != 0
 }
 
-macro_rules! shift_register_bits {
-    ($bits:expr) => {
-        process_mask($bits, bp32![8], bp32![11])
-    }
+fn shift_type_bits(bits: u32) -> u32 {
+    mask![bits, 5, 6]
 }
 
+fn shift_register_bits(bits: u32) -> u32 {
+    mask![bits, 8, 11]
+}
+
+fn shift_constant_bits(bits: u32) -> u32 { mask![bits, 7, 11] }
+
+
 fn execute_shift(operand: u32, shift_amount: u32, shift_opcode: ShiftOp,
-    c_bit: &mut u8) -> u32 {
+                 c_bit: &mut u8) -> u32 {
     let mut result: u32;
-    
+    let mut cbit: u64;
     match shift_opcode {
         ShiftOp::LSL => {
             result = operand << shift_amount;
             // CPSR c bit is set to bit 32 after lsl (carry out)
-            let cbit = ( ((operand as u64) << shift_amount) >> 32 ) & 1;
-            *c_bit = cbit as u8;
+            cbit = (((operand as u64) << shift_amount as u64) >> 32) & 1;
         }
         ShiftOp::LSR => {
             result = operand >> shift_amount;
-            let cbit = (operand >> (shift_amount - 1)) & 1;
-            *c_bit = cbit as u8;
+            cbit = ( (operand >> (shift_amount - 1)) & 1 ) as u64;
         }
         ShiftOp::ASR => {
-            // TODO: result = arithmetic_shift_right(operand, shift_amount);
-            ////bit (shift_amount - 1) is the carry out
-            //*c_bit = (operand >> (shift_amount - 1)) & 1;
+            result = arithmetic_shift_right(operand, shift_amount);
+            cbit = ( (operand >> (shift_amount - 1)) & 1 ) as u64;
         }
         ShiftOp::ROR => {
-            // TODO: result = rotate_right(operand, shift_amount);
-            ////bit (shift_amount - 1) is the carry out
-            //*c_bit = (operand >> (shift_amount - 1)) & 1;
+            result = rotate_right(operand, shift_amount);
+            cbit = ( (operand >> (shift_amount - 1)) & 1 ) as u64;
         }
     };
-    0
+    *c_bit = cbit as u8;
+    result
 }
 
-pub fn reg_offset_shift(cpu: &CpuState, instr: &mut Instruction, c_bit: u8) -> u32 {
+pub fn reg_offset_shift(cpu: &CpuState, instr: &mut Instruction, mut c_bit: u8) -> u32 {
     let mut result: u32 = 0;
     let bits = instr.code;
     let reg_contents: u32 = cpu.registers[shifted_reg_m_bits(bits) as usize];
 
     if shift_mode_bit(bits) {
-        let lower_byte: u8 = cpu.registers[shift_register_bits![bits] as usize] as u8;
-        // TODO: result = execute_shift(reg_contents, lower_byte, SHIFT_TYPE_BITS(instr->code), c_bit);
+        let lower_byte: u8 = cpu.registers[shift_register_bits(bits) as usize] as u8;
+        let shift_type = shift_type_bits(bits);
+        let shift_type = FromPrimitive::from_u32(shift_type).unwrap();
+        result = execute_shift(reg_contents, lower_byte as u32, shift_type, &mut c_bit);
     } else {
-        // TODO: result = execute_shift(reg_contents, SHIFT_CONSTANT_BITS(instr->code), 
-            //SHIFT_TYPE_BITS(instr->code), c_bit); 
+        let shift_type = shift_type_bits(bits);
+        let shift_type = FromPrimitive::from_u32(shift_type).unwrap();
+        result = execute_shift(reg_contents, shift_constant_bits(bits), shift_type, &mut c_bit);
     }
 
     result
 }
+
+fn rotate_right(operand: u32, rotate_amount: u32) -> u32 {
+    let mut result: u32 = operand >> rotate_amount;
+    result |= operand << (32 - rotate_amount);
+
+    result
+}
+
+fn arithmetic_shift_right(operand: u32, shift_amount: u32) -> u32 {
+    let mut result: u32 = 0;
+    if ((1 << 31) & operand) != 0 {
+        // MSB is 1
+        result = operand >> shift_amount;
+        for i in 0..shift_amount {
+            result |= 1 << (31 - 1);
+        }
+    } else {
+        result = operand >> shift_amount;
+    }
+    result
+}
+
+
+
